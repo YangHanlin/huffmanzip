46244182c45adf7ecc642ca49cef3eb934b29db1
YangHanlin <mattoncis@hotmail.com>
2019-05-13 23:03:37 +0800

    Update huffmanzip.cpp

--- huffmanzip.cpp
+++ huffmanzip.cpp
@@ -1,10 +1,12 @@
 // huffmanzip.cpp
 
+#include "Settings.h"
+
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[]) {
-    // ...
+    parseArgs(argc, argv);
 }
