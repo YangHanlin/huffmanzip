66c894a593364a2e6ab033a67911dbe1b39b3fff
YangHanlin <mattoncis@hotmail.com>
2019-05-14 00:07:30 +0800

    Update the source of programName
    After the update in Settings.h & Settings.cpp

--- Util.cpp
+++ Util.cpp
@@ -16,11 +16,11 @@ using std::runtime_error;
 void sendMessage(MessageType type, const string &msg) {
     switch (type) {
         case MSG_INFO:
-            cout << globalSettings.programName << ": info: " << msg << endl;
+            cout << sessionSettings.programName << ": info: " << msg << endl;
         case MSG_WARNING:
-            cerr << globalSettings.programName << ": warning: " << msg << endl;
+            cerr << sessionSettings.programName << ": warning: " << msg << endl;
         case MSG_ERROR:
-            cerr << globalSettings.programName << ": error: " << msg << endl;
+            cerr << sessionSettings.programName << ": error: " << msg << endl;
         default:
             throw runtime_error("Invalid MessageType (enum) value in sendMessage()");
     }
