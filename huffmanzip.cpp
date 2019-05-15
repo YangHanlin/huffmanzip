b96491181c778dc8e1f96cbe772f2b0470a6a261
YangHanlin <mattoncis@hotmail.com>
2019-05-15 14:03:12 +0800

    Update huffmanzip.cpp
    A more formal main module

--- huffmanzip.cpp
+++ huffmanzip.cpp
@@ -3,34 +3,11 @@
 #include "Settings.h"
 #include "Util.h"
 
-// #include <iostream>
 #include <stdexcept>
 
-// using std::cerr;
-// using std::endl;
 using std::exception;
 
 int main(int argc, char *argv[]) {
-    // argc = 1;
-    // char *argvCandidate[] = {
-    //     "huffmanzip.exe"
-    // };
-    // argv = argvCandidate;
-    // cout << "Original:" << endl
-    //      << "globalSettings.compressorIdentifier = \"" << string(globalSettings.compressorIdentifier, globalSettings.compressorIdentifier + GlobalSettings::COMPRESSOR_IDENTIFIER_SIZE) << "\"" << endl
-    //      << "globalSettings.compressorVersion = " << globalSettings.compressorVersion << endl
-    //      << "globalSettings.compressorVersionStr = \"" << globalSettings.compressorVersionStr << "\"" << endl
-    //      << "globalSettings.fileSignature = " << globalSettings.fileSignature << endl
-    //      << "sessionSettings.programName = \"" << sessionSettings.programName << "\"" << endl
-    //      << "sessionSettings.showHelp = " << sessionSettings.showHelp << endl
-    //      << "sessionSettings.showVersion = " << sessionSettings.showVersion << endl
-    //      << "sessionSettings.compress = " << sessionSettings.compress << endl
-    //      << "sessionSettings.verboseMode = " << sessionSettings.verboseMode << endl
-    //      << "sessionSettings.keepOriginalFile = " << sessionSettings.keepOriginalFile << endl
-    //      << "sessionSettings.useStdin = " << sessionSettings.useStdin << endl
-    //      << "sessionSettings.useStdout = " << sessionSettings.useStdout << endl
-    //      << "sessionSettings.inFilePath = \"" << sessionSettings.inFilePath << "\"" << endl
-    //      << "sessionSettings.outFilePath = \"" << sessionSettings.outFilePath << "\"" << endl;
     try {
         parseArgs(argc, argv);
         if (sessionSettings.showHelp) {
@@ -41,27 +18,10 @@ int main(int argc, char *argv[]) {
             showVersion();
             return 0;
         }
-        sendMessage(MSG_WARNING, "the feature has not been implemented; please wait");
+        sendMessage(MSG_INFO, "This feature has not been implemented yet; please wait");
         return 0;
     } catch (const exception &e) {
-        sendMessage(MSG_INFO, "program terminated due to an error");
+        sendMessage(MSG_INFO, "Program terminated due to an error");
+        return 1;
     }
-    // cout << "Parsed:" << endl
-    //      << "globalSettings.compressorIdentifier = \"" << string(globalSettings.compressorIdentifier, globalSettings.compressorIdentifier + GlobalSettings::COMPRESSOR_IDENTIFIER_SIZE) << "\"" << endl
-    //      << "globalSettings.compressorVersion = " << globalSettings.compressorVersion << endl
-    //      << "globalSettings.compressorVersionStr = \"" << globalSettings.compressorVersionStr << "\"" << endl
-    //      << "globalSettings.fileSignature = " << globalSettings.fileSignature << endl
-    //      << "sessionSettings.programName = \"" << sessionSettings.programName << "\"" << endl
-    //      << "sessionSettings.showHelp = " << sessionSettings.showHelp << endl
-    //      << "sessionSettings.showVersion = " << sessionSettings.showVersion << endl
-    //      << "sessionSettings.compress = " << sessionSettings.compress << endl
-    //      << "sessionSettings.verboseMode = " << sessionSettings.verboseMode << endl
-    //      << "sessionSettings.keepOriginalFile = " << sessionSettings.keepOriginalFile << endl
-    //      << "sessionSettings.useStdin = " << sessionSettings.useStdin << endl
-    //      << "sessionSettings.useStdout = " << sessionSettings.useStdout << endl
-    //      << "sessionSettings.inFilePath = \"" << sessionSettings.inFilePath << "\"" << endl
-    //      << "sessionSettings.outFilePath = \"" << sessionSettings.outFilePath << "\"" << endl;
-    // showHelp();
-    // showVersion();
-    // system("pause");
 }
