1e30a63a5b7f5cc20acf5a078f92c6f8f30f8b92
YangHanlin <mattoncis@hotmail.com>
2019-05-19 09:12:37 +0800

    Add assistive tool for debugging

--- huffmanzip.cpp
+++ huffmanzip.cpp
@@ -1,5 +1,7 @@
 // huffmanzip.cpp
 
+#define DIRECT_DEBUG
+
 #include "Settings.h"
 #include "Util.h"
 #include "CompressCore.h"
@@ -9,6 +11,15 @@
 using std::exception;
 
 int main(int argc, char *argv[]) {
+    #ifdef DIRECT_DEBUG
+    char *argvCandidate[] = {
+        ".\\huffmanzip.exe",
+        "D:\\matto\\Documents\\Codes\\Homework\\DS-9p\\eng.easy.txt.test"
+        "-c"
+    };
+    argc = sizeof(argvCandidate) / sizeof(argvCandidate[0]);
+    argv = argvCandidate;
+    #endif
     try {
         parseArgs(argc, argv);
         if (sessionSettings.showHelp) {
